55639c3198d706ce75870818d094586ed39810fb
Bharath Km <bharathkm37@gmail.com>
2025-01-30 21:58:30 +0530

    Form fields added

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const PaymentForm = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', jerseyNumber: '', playerType: 'Batsman' });
+  const [formData, setFormData] = useState({ name: '', teamname:'', email: '', jerseyNumber: '', jerseySize: '', playerType: 'Batsman' });
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -22,12 +22,26 @@ const PaymentForm = () => {
           <input type="text" className="form-control" name="name" onChange={handleChange} required />
         </div>
         <div className="mb-3">
+          <label className="form-label">Team Name</label>
+          <input type="text" className="form-control" name="teamname" onChange={handleChange} required />
+        </div>
+        <div className="mb-3">
           <label className="form-label">Email</label>
           <input type="email" className="form-control" name="email" onChange={handleChange} required />
         </div>
         <div className="mb-3">
           <label className="form-label">Jersey Number</label>
           <input type="number" className="form-control" name="jerseyNumber" onChange={handleChange} required />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Jersey Type</label>
+          <select className="form-select" name="jerseySize" onChange={handleChange}>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
+          </select>
         </div>
         <div className="mb-3">
           <label className="form-label">Player Type</label>
